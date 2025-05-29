a9cb16300ef1c947bff58f367fce7bda3ad6d738
Afriza22 <159934899+Afriza22@users.noreply.github.com>
2025-05-29 22:20:44 +0700

    Add files via upload

---
+++ Tugas Akhir PAW/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Babysteps</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+  </head>
+  <body>
+
+    <div class="content-top" id="img-top">
+        <div class="row">
+            <div class="col-6">
+            </div>
+            <div class="col-6 d-flex align-items-center" style="min-height: 100vh;">
+                <div class="">
+                    <h2 class="text-primary">BABYSTEPS</h2>
+                    <p>Inovasi Digital untuk Mitigasi Risiko Stunting</p>
+                    <a href="login.php" class="btn btn-warning mt-4">LOGIN</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  
+    <style>
+        #img-top {
+            background-image: url('img/bg-top.svg');
+        }
+        .content-top {
+            min-height: 100vh;
+            
+        }
+    </style>
+    </body>
+</html>
